e2afc0b2d605429be94f30fcb0fbd4b7b347771a
Ryan Donohue <ryan@rdonohue.ca>
2018-08-08 14:22:39 -0600

    refactor(fileUtils): Removing redundant code, added const file name

--- trust-provider/fileUtils.go
+++ trust-provider/fileUtils.go
@@ -13,21 +13,15 @@ type devDBRecord struct {
 	Token   string      `json:"token"`
 }
 
+const DbFilePath = "./db.json"
+
 func createDevDB() error {
-	path, err := filepath.Abs(".")
-	if err != nil {
-		log.Println(err)
-		return err
-	}
 
-	path = path + "/db.json"
-
-	_, err = os.Stat(path)
+	_, err := os.Stat(DbFilePath)
 
 	if os.IsNotExist(err) {
-		var file, err = os.Create(path)
+		var file, err = os.Create(DbFilePath)
 		if err != nil {
-			log.Println(err)
 			return err
 		}
 		defer file.Close()
@@ -40,14 +34,14 @@ func appendFile(account interface{}, token string) error {
 
 	err := createDevDB()
 	if err != nil {
-		log.Printf("failed opening file1: %s", err)
+		log.Printf("Error creating file: %s", err)
 		return err
 	}
 
-	path, err := filepath.Abs("./db.json")
-	file, err := os.OpenFile(path, os.O_APPEND|os.O_RDWR, 0644)
+	path, err := filepath.Abs(DbFilePath)
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_APPEND, 0644)
 	if err != nil {
-		log.Printf("failed opening file2: %s", err)
+		log.Printf("Error opening file: %s", err)
 		return err
 	}
 
@@ -61,18 +55,16 @@ func appendFile(account interface{}, token string) error {
 
 	json.Unmarshal(fileContents, &records)
 
-	var record = devDBRecord{
+	record := devDBRecord{
 		Account: account,
 		Token:   token,
 	}
 
 	records = append(records, record)
 	r, err := json.Marshal(records)
-
-	file.Write(nil)
 	_, err = file.Write(r)
 	if err != nil {
-		log.Fatalf("failed writing to file: %s", err)
+		log.Fatalf("Error writing to file: %s", err)
 	}
 
 	return err
