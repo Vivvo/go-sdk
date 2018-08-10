afaa168efabae117cd827aa9faa3e50985b429fb
Lucas Tétreault <lucastetreault@gmail.com>
2018-08-09 20:10:59 -0600

    docs(TrustProvider): Update DMV Sample

--- docs/tutorials/buildAndIntegrateTrustProvider/DMVTrustProvider.go
+++ docs/tutorials/buildAndIntegrateTrustProvider/DMVTrustProvider.go
@@ -3,39 +3,30 @@ package main
 import (
 	"github.com/Vivvo/go-sdk/trust-provider"
 	"log"
-	"github.com/satori/go.uuid"
 	"encoding/csv"
 	"os"
 	"strings"
 	"github.com/pkg/errors"
 	"time"
+	"github.com/satori/go.uuid"
 )
-
-type DMVAccount struct {
-	CustomerNumber   string `json:"customerNumber"`
-	ValidationNumber string `json:"validationNumber"`
-	FirstName        string `json:"firstName"`
-	LastName         string `json:"lastName"`
-	BirthDate        string `json:"birthDate"`
-	Token            string `json:"token"`
-}
 
 type DMVAccountManager struct{}
 
 func (d *DMVAccountManager) Update(account interface{}, token uuid.UUID) error {
-	reader, _ := os.Open("./drones.csv")
+	reader, _ := os.Open("./DMVUsers.csv")
 	r := csv.NewReader(reader)
 	records, _ := r.ReadAll()
 	reader.Close()
 
 	if a, ok := account.(DMVAccount); ok {
 		for _, r := range records[1:] {
-			if strings.Compare(r[0], a.MemberNumber) == 0 {
-				r[4] = token.String()
+			if strings.Compare(r[0], a.CustomerNumber) == 0 {
+				r[5] = token.String()
 			}
 		}
 
-		file, _ := os.OpenFile("./drones.csv", os.O_RDWR, 0777)
+		file, _ := os.OpenFile("./DMVUsers.csv", os.O_RDWR, 0777)
 		writer := csv.NewWriter(file)
 		writer.WriteAll(records)
 		defer file.Close()
@@ -46,28 +37,37 @@ func (d *DMVAccountManager) Update(account interface{}, token uuid.UUID) error {
 }
 
 func (d *DMVAccountManager) Read(token uuid.UUID) (interface{}, error) {
-	reader, _ := os.Open("./drones.csv")
+	reader, _ := os.Open("./DMVUsers.csv")
 	defer reader.Close()
 	r := csv.NewReader(reader)
 	records, _ := r.ReadAll()
 
 	for _, r := range records[1:] {
-		if strings.Compare(r[4], token.String()) == 0 {
-			return DMVAccount{MemberNumber: r[0], Status: r[1], FirstName: r[2], LastName: r[3], Token: r[4]}, nil
+		if strings.Compare(r[5], token.String()) == 0 {
+			return DMVAccount{CustomerNumber: r[0], ValidationNumber: r[1], FirstName: r[2], LastName: r[3], BirthDate: r[4], Token: r[5]}, nil
 		}
 	}
 	return nil, errors.New("not match found")
 }
 
+type DMVAccount struct {
+	CustomerNumber   string `json:"customerNumber"`
+	ValidationNumber string `json:"validationNumber"`
+	FirstName        string `json:"firstName"`
+	LastName         string `json:"lastName"`
+	BirthDate        string `json:"birthDate"`
+	Token            string `json:"token"`
+}
+
 func onboarding(s map[string]string, n map[string]float64, b map[string]bool) (interface{}, error) {
-	reader, _ := os.Open("./drones.csv")
+	reader, _ := os.Open("./DMVUsers.csv")
 	defer reader.Close()
 	r := csv.NewReader(reader)
 	records, _ := r.ReadAll()
 
 	for _, r := range records[1:] {
-		if strings.Compare(r[0], s["memberNumber"]) == 0 && strings.Compare(r[2], s["firstName"]) == 0 && strings.Compare(r[3], s["lastName"]) == 0 {
-			return DMVAccount{MemberNumber: r[0], Status: r[1], FirstName: r[2], LastName: r[3], Token: r[4]}, nil
+		if strings.Compare(r[0], s["customerNumber"]) == 0 && strings.Compare(r[1], s["validationNumber"]) == 0 && strings.Compare(r[2], s["firstName"]) == 0 && strings.Compare(r[3], s["lastName"]) == 0 {
+			return DMVAccount{CustomerNumber: r[0], ValidationNumber: r[1], FirstName: r[2], LastName: r[3], BirthDate: r[4], Token: r[5]}, nil
 		}
 	}
 	return nil, errors.New("no match found")
