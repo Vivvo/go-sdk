6e9321f6987495d1cfafd58940d9595b97a4ec78
Mandeep Singh <mandeep@mandeepsingh.ca>
2018-11-21 10:46:10 -0600

    feat(OCap): Add the ability to verify Ocaps

--- did/ObjectCapability.go
+++ did/ObjectCapability.go
@@ -1,10 +1,13 @@
 package did
 
 import (
+	"log"
+	"crypto"
 	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"github.com/Vivvo/go-sdk/utils"
-	"log"
 	"time"
 )
 
@@ -47,3 +50,67 @@ func (c *Capability) Sign(privateKey *rsa.PrivateKey) (*ObjectCapability, error)
 		Proof: p,
 	}, nil
 }
+
+
+func (c *ObjectCapability) Verify(resolver ResolverInterface) error {
+
+	// need to get the resolver to get the person who signed it so we can go to the block chain and get the issuers public key....
+	didDocument, err := resolver.Resolve(c.Proof.Creator)
+	if err != nil {
+		return err
+	}
+
+	// Find the public key that the claim is using
+	pubKey, err := didDocument.GetPublicKeyById(c.Proof.Creator)
+	if err != nil {
+		return err
+	}
+
+	sig := c.Proof.SignatureValue
+
+	options := utils.Proof{Created: c.Proof.Created, Creator: c.Proof.Creator}
+	c.Proof = nil
+
+	credJson, err := utils.Canonicalize(c)
+	if err != nil {
+		return err
+	}
+
+	optionsJson, err := utils.Canonicalize(options)
+	if err != nil {
+		return err
+	}
+
+	// Build hash string
+	h := sha256.New()
+	_, err = h.Write(credJson)
+	if err != nil {
+		return err
+	}
+
+	optionsHash := sha256.New()
+	_, err = optionsHash.Write(optionsJson)
+	if err != nil {
+		return err
+	}
+
+	hashString := append(h.Sum(nil), optionsHash.Sum(nil)...)
+	h = sha256.New()
+	_, err = h.Write(hashString)
+	if err != nil {
+		return err
+	}
+
+	decodedSig, err := base64.URLEncoding.DecodeString(sig)
+	if err != nil {
+		return err
+	}
+
+	err = rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, h.Sum(nil), decodedSig)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
--- utils/jsonLd.go
+++ utils/jsonLd.go
@@ -67,3 +67,78 @@ func Sign(o interface{}, proof *Proof, privateKey *rsa.PrivateKey) (interface{},
 
 	return o, proof, nil
 }
+//
+//func Verify(o interface{}, types []string, nonce string, resolver ResolverInterface) error {
+//
+//	if nonce != "" && strings.Compare(nonce, vc.Proof.Nonce) != 0 {
+//		return errors.New("invalid nonce")
+//	}
+//
+//	//FIXME: We must always validate at least one type - otherwise the consumer may not be sure the claim
+//	// says what they think it says...
+//	if len(types) > 0 {
+//		for i, t := range types {
+//			if strings.Compare(vc.Type[i], t) != 0 {
+//				return errors.New(fmt.Sprintf("missing type %s", t))
+//			}
+//		}
+//	}
+//
+//	didDocument, err := resolver.Resolve(vc.Issuer)
+//	if err != nil {
+//		return err
+//	}
+//
+//	// Find the public key that the claim is using
+//	pubKey, err := didDocument.GetPublicKeyById(vc.Proof.Creator)
+//	if err != nil {
+//		return err
+//	}
+//
+//	sig := vc.Proof.SignatureValue
+//
+//	options := utils.Proof{Created: vc.Proof.Created, Creator: vc.Proof.Creator, Nonce: vc.Proof.Nonce}
+//	vc.Proof = nil
+//
+//	credJson, err := utils.Canonicalize(vc)
+//	if err != nil {
+//		return err
+//	}
+//
+//	optionsJson, err := utils.Canonicalize(options)
+//	if err != nil {
+//		return err
+//	}
+//
+//	// Build hash string
+//	h := sha256.New()
+//	_, err = h.Write(credJson)
+//	if err != nil {
+//		return err
+//	}
+//
+//	optionsHash := sha256.New()
+//	_, err = optionsHash.Write(optionsJson)
+//	if err != nil {
+//		return err
+//	}
+//
+//	hashString := append(h.Sum(nil), optionsHash.Sum(nil)...)
+//	h = sha256.New()
+//	_, err = h.Write(hashString)
+//	if err != nil {
+//		return err
+//	}
+//
+//	decodedSig, err := base64.URLEncoding.DecodeString(sig)
+//	if err != nil {
+//		return err
+//	}
+//
+//	err = rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, h.Sum(nil), decodedSig)
+//	if err != nil {
+//		return err
+//	}
+//
+//	return nil
+//}
