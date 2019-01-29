8256169267c55e5e2b4b76addf48686a76e5c47c
Mandeep Singh <mandeep@mandeepsingh.ca>
2019-01-29 17:39:59 -0600

    fix: Make a seperate generate ddoc for mobile-sdk

--- did/GenerateDidDocument.go
+++ did/GenerateDidDocument.go
@@ -12,7 +12,7 @@ type GenerateDidDocument struct {
 
 }
 
-func (g *GenerateDidDocument) Generate(id string) (*Document, error) {
+func (g *GenerateDidDocument) GenerateDDoc(id string) (*Document, error) {
 	var doc Document
 	doc.Context = "https://w3id.org/did/v1"
 	doc.Id = id
@@ -54,6 +54,61 @@ func (g *GenerateDidDocument) Generate(id string) (*Document, error) {
 	}
 
 	err = g.W.Dids().Create(doc.Id, string(docJson), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = g.Resolver.Register(&doc)
+	if err != nil {
+		return nil, err
+	}
+
+	return &doc, err
+}
+
+
+func (g *GenerateDidDocument) Generate(id string, w *wallet.Wallet) (*Document, error) {
+	var doc Document
+	doc.Context = "https://w3id.org/did/v1"
+	doc.Id = id
+
+	rsaPublicKey, err := w.Crypto().GenerateRSAKey("RsaVerificationKey2018", id)
+	if err != nil {
+		return nil, err
+	}
+
+	ed25519PublicKey, err := w.Crypto().GenerateEd25519Key("Ed25519KeyExchange2018", id)
+	if err != nil {
+		return nil, err
+	}
+
+	pubKey := PublicKey{
+		Owner:        id,
+		Id:           fmt.Sprintf("%s#keys-1", id),
+		T:            "RsaVerificationKey2018",
+		PublicKeyPem: rsaPublicKey,
+	}
+
+	pubKey2 := PublicKey{
+		Owner:           id,
+		Id:              fmt.Sprintf("%s#keys-2", id),
+		T:               "Ed25519KeyExchange2018",
+		PublicKeyBase58: ed25519PublicKey,
+	}
+
+	doc.PublicKey = []PublicKey{pubKey, pubKey2}
+
+	auth := Authentication{}
+	auth.PublicKey = pubKey.Id
+	auth.T = "RsaSignatureAuthentication2018"
+	doc.Authentication = []Authentication{auth}
+
+	docJson, err := json.Marshal(doc)
+	if err != nil {
+		return nil, err
+	}
+
+	err = w.Dids().Create(doc.Id, string(docJson), nil)
 	if err != nil {
 		return nil, err
 	}
