567b86e62f4a7eb7a693e64ac00e3049a5fbcc24
Taylor Petrychyn <tpetrychyn@gmail.com>
2019-02-08 13:38:05 -0600

    fix(generateDid): simplify generateDid in to a single function

--- did/GenerateDidDocument.go
+++ did/GenerateDidDocument.go
@@ -9,63 +9,8 @@ import (
 type GenerateDidDocument struct {
 	Resolver ResolverInterface
 }
-type GenerateDidDocumentMobile struct {
-	Resolver ResolverInterface
-	W        *wallet.Wallet
-}
 
-func (g *GenerateDidDocumentMobile) GenerateDDoc(id string, serviceEndpoints []Service) (*Document, error) {
-	var doc Document
-	doc.Context = "https://w3id.org/did/v1"
-	doc.Id = id
-
-	rsaPublicKey, err := g.W.Crypto().GenerateRSAKey("RsaVerificationKey2018", id)
-	if err != nil {
-		return nil, err
-	}
-
-	ed25519PublicKey, err := g.W.Crypto().GenerateEd25519Key("Ed25519KeyExchange2018", id)
-	if err != nil {
-		return nil, err
-	}
-
-	pubKey := PublicKey{
-		Owner:        id,
-		Id:           fmt.Sprintf("%s#keys-1", id),
-		T:            "RsaVerificationKey2018",
-		PublicKeyPem: rsaPublicKey,
-	}
-
-	pubKey2 := PublicKey{
-		Owner:           id,
-		Id:              fmt.Sprintf("%s#keys-2", id),
-		T:               "Ed25519KeyExchange2018",
-		PublicKeyBase58: ed25519PublicKey,
-	}
-
-	doc.PublicKey = []PublicKey{pubKey, pubKey2}
-
-	auth := Authentication{}
-	auth.PublicKey = pubKey.Id
-	auth.T = "RsaSignatureAuthentication2018"
-	doc.Authentication = []Authentication{auth}
-
-	doc.Service = serviceEndpoints
-
-	docJson, err := json.Marshal(doc)
-	if err != nil {
-		return nil, err
-	}
-
-	err = g.W.Dids().Create(doc.Id, string(docJson), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return &doc, err
-}
-
-func (g *GenerateDidDocument) Generate(id string, w *wallet.Wallet) (*Document, error) {
+func (g *GenerateDidDocument) Generate(id string, w *wallet.Wallet, serviceEndpoints ...Service) (*Document, error) {
 	var doc Document
 	doc.Context = "https://w3id.org/did/v1"
 	doc.Id = id
@@ -100,6 +45,8 @@ func (g *GenerateDidDocument) Generate(id string, w *wallet.Wallet) (*Document, 
 	auth.PublicKey = pubKey.Id
 	auth.T = "RsaSignatureAuthentication2018"
 	doc.Authentication = []Authentication{auth}
+
+	doc.Service = serviceEndpoints
 
 	docJson, err := json.Marshal(doc)
 	if err != nil {
