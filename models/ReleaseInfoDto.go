d09876628b3eee8e0e07454d092b5445f52533f4
Connor Brezinsky <connor.brezinsky@gmail.com>
2019-04-25 11:12:02 -0600

    refactor(versioning.go & RelaseInfoDto.go): fix spelling mistake, change json values to lowercase

--- models/ReleaseInfoDto.go
+++ models/ReleaseInfoDto.go
@@ -1,9 +1,10 @@
 package models
 
+// ReleaseInfoDto holds our application release information
 type ReleaseInfoDto struct {
-	VersionNumber string `json:"VERSION_NUMBER"`
-	AppName       string `json:"APP_NAME"`
-	GitShaSort    string `json:"GIT_SHA_SHORT"`
-	TenantName    string `json:"TENANT_NAME"`
-	StaredOn      string  `json:"STARTED_ON"`
+	VersionNumber string `json:"version_number"`
+	AppName       string `json:"app_name"`
+	GitShaSort    string `json:"git_sha_short"`
+	TenantName    string `json:"tenant_name"`
+	StartedOn     string `json:"started_one"`
 }
