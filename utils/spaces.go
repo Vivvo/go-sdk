f6dc45b09ba745a5fab993bca44fbe6bc6b1d474
connorbrez <connor.brezinsky@gmail.com>
2019-06-11 17:07:21 -0600

    fix(spaces): use s3UploaderManager instead of putObject

--- utils/spaces.go
+++ utils/spaces.go
@@ -1,49 +1,42 @@
 package utils
 
 import (
+	"io"
 	"log"
-	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 type SpaceConfig struct {
-	S3Instance *s3.S3
-	Bucket     string
+	S3Session *session.Session
+	Bucket    string
 }
 
 // NewS3Instance creates a new S3 instance with the specifed endpoint & region.
 // It requires a Client Key & Secret
-func NewS3Instance(endpoint, region, key, secret string) *s3.S3 {
+func NewS3Session(endpoint, region, key, secret string) *session.Session {
 	s3Config := &aws.Config{
 		Credentials: credentials.NewStaticCredentials(key, secret, ""),
 		Endpoint:    aws.String(endpoint),
 		Region:      aws.String(region), // This is counter intuitive, but it will fail with a non-AWS region name.
 	}
-	return s3.New(session.New(s3Config))
-}
-
-// ListBuckets will list all storage buckets
-func (s *SpaceConfig) ListBuckets() ([]*s3.Bucket, error) {
-	spaces, err := s.S3Instance.ListBuckets(nil)
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-	return spaces.Buckets, nil
+	return session.Must(session.NewSession(s3Config))
 }
 
 // Upload will upload the content with the specifed key to the specifed bucket
-func (s *SpaceConfig) Upload(content, key string) error {
-	object := s3.PutObjectInput{
-		Body:   strings.NewReader(content),
+func (s *SpaceConfig) Upload(content io.Reader, key string) error {
+
+	uploader := s3manager.NewUploader(s.S3Session)
+
+	object := &s3manager.UploadInput{
+		Body:   content,
 		Bucket: aws.String(s.Bucket),
 		Key:    aws.String(key),
 	}
-	_, err := s.S3Instance.PutObject(&object)
+	_, err := uploader.Upload(object)
 	if err != nil {
 		log.Println(err)
 		return err
