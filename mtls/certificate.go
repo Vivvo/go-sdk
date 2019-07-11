b8867c3418a98f015b5244c880a17f6a2c782faf
jjamieson1 <jjamie@gmail.com>
2019-07-11 09:57:20 -0600

    feat(Mutual TLS): Package to enable mutual authentication using the vivvo-ca
    This package allows for pulling in the public CA, creating certificates and sending out those
    certificates for authorization/signing from the CA

---
+++ mtls/certificate.go
@@ -0,0 +1,105 @@
+package mtls
+
+import (
+"crypto/rand"
+"crypto/rsa"
+"crypto/tls"
+"crypto/x509"
+"encoding/json"
+"encoding/pem"
+"github.com/go-resty/resty"
+"io/ioutil"
+"log"
+"os"
+)
+
+func RetrieveMutualAuthCertificate(signRequest SignRequest) tls.Certificate {
+	if _, err := os.Stat("client.crt"); os.IsNotExist(err) {
+
+		privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+		publicKey := &privateKey.PublicKey
+
+		a := x509.MarshalPKCS1PublicKey(publicKey)
+		certificateToSign := ClientCertificate {
+			Certificate: a,
+		}
+
+		requestBody, err := json.Marshal(certificateToSign)
+		response, err := resty.R().
+			SetHeader("Content-Type", "application/json").
+			SetBody(requestBody).
+			SetHeader("cn", signRequest.CommonName).
+			SetHeader("Authorization", signRequest.Authorization).
+			Post(signRequest.CertificateAuthorityUrl + "/api/v1/sign")
+
+		if err != nil {
+			log.Fatal("Error calling CA for signing certificate, error: ", err.Error())
+		}
+
+		var signedCert ClientCertificate
+		json.Unmarshal(response.Body(), &signedCert)
+
+		//Public key
+		certOut, err := os.Create("client.crt")
+		pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: signedCert.Certificate})
+		certOut.Close()
+
+		// Private key
+		keyOut, err := os.OpenFile("client.key", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+		pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)})
+		keyOut.Close()
+	}
+	return loadSignedClientCert()
+
+}
+
+func RetrieveCaCertificate(request SignRequest) []byte {
+	if _, err := os.Stat("ca.crt"); os.IsNotExist(err) {
+
+		response, err := resty.R().
+			SetHeader("Content-Type", "application/json").
+			Get(request.CertificateAuthorityUrl + "/api/v1/cert")
+
+		if err != nil {
+			log.Printf("Error calling CA for CA certificate, error: %s", err.Error())
+		}
+
+		var caCert ClientCertificate
+		json.Unmarshal(response.Body(), &caCert)
+
+		//Public key
+		certOut, err := os.Create("ca.crt")
+		pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: caCert.Certificate})
+		certOut.Close()
+		log.Print("written ca.crt\n")
+
+	}
+	return loadCACert()
+}
+
+func loadSignedClientCert() tls.Certificate {
+	catls, err := tls.LoadX509KeyPair("client.crt", "client.key")
+	if err != nil {
+		panic(err)
+	}
+	return  catls
+}
+
+func loadCACert() []byte {
+	caCert, err := ioutil.ReadFile("ca.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return caCert
+}
+
+type ClientCertificate struct {
+	Certificate 	[]byte	`json:"certificate"`
+}
+
+
+type SignRequest struct {
+	CommonName string
+	CertificateAuthorityUrl	string
+	Authorization 	string
+}
