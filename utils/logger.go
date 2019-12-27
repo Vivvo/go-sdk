1347e5f1debcc0509c3676869fe3e92664c5a3e0
Brett Mayson <brett@vivvo.com>
2019-12-27 09:37:18 -0600

    feat(logging): always use JSON

--- utils/logger.go
+++ utils/logger.go
@@ -22,6 +22,7 @@ func getLogger() *zap.Logger {
 		} else {
 			config = zap.NewProductionConfig()
 		}
+		config.Encoding = "json"
 		config.OutputPaths = []string{"stdout"}
 		logger, _ = config.Build()
 	}
