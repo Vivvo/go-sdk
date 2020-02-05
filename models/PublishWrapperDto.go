4c7bc0ab92785abf527a84d4ca542f96147f3edb
Austin McPhail <austin@vivvo.com>
2020-02-05 13:03:14 -0600

    refactor([RDNG-515]): added encryption fields to wrapper

--- models/PublishWrapperDto.go
+++ models/PublishWrapperDto.go
@@ -1,6 +1,8 @@
 package models
 
 type PublishWrapperDto struct {
-	EventType string      `json:"eventType"`
-	Data      interface{} `json:"data"`
+	EventType      string      `json:"eventType"`
+	Data           interface{} `json:"data"`
+	EncryptedNonce string      `json:"encryptedNonce,omitempty"`
+	EncryptedKey   string      `json:"encryptedKey,omitempty"`
 }
