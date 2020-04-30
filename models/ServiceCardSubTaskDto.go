6c8bcbf9cb1aea72ee1f1b50e1de724a8a425b29
Taylor Petrychyn <tpetrychyn@gmail.com>
2020-04-30 10:19:54 -0600

    fix(serviceCardTaskDto): add Action field to taskDtto

--- models/ServiceCardSubTaskDto.go
+++ models/ServiceCardSubTaskDto.go
@@ -1,21 +1,21 @@
-package models;
-
+package models
 
 //Service Card Task Type - The type of task
 //CONTAINER - The main structure of the task.  The big text section on the task
 //ACTION - The action of the task -> A link where it points to
 //FORM - A link to the form used in this sub-task
 type ServiceCardTaskType string
+
 const (
 	CONTAINER ServiceCardTaskType = "CONTAINER"
 	ACTION    ServiceCardTaskType = "ACTION"
 	FORM      ServiceCardTaskType = "FORM"
 )
 
-
 //Service Card Task Style - Style is in form of bootstrap styles
 //Example: https://getbootstrap.com/docs/4.4/components/alerts/
 type ServiceCardTaskStyle string
+
 const (
 	PRIMARY ServiceCardTaskStyle = "PRIMARY"
 	SUCCESS ServiceCardTaskStyle = "SUCCESS"
@@ -28,8 +28,8 @@ const (
 type ServiceCardSubTaskDto struct {
 	Label       string                 `json:"label"`
 	Description string                 `json:"description"`
-	Style       ServiceCardTaskStyle          `json:"style"`
+	Style       ServiceCardTaskStyle   `json:"style"`
 	Action      string                 `json:"action"`
-	Type        ServiceCardTaskType           `json:"type"`
+	Type        ServiceCardTaskType    `json:"type"`
 	Params      map[string]interface{} `json:"params"`
 }
--- models/ServiceCardTaskDto.go
+++ models/ServiceCardTaskDto.go
@@ -1,12 +1,12 @@
 package models
 
-
 //Service Card Task Structure - The main structure of a task
 type ServiceCardTaskDto struct {
-	Label       string                 `json:"label"`
-	Description string                 `json:"description"`
-	Style       ServiceCardTaskStyle          `json:"style"`
-	Type        ServiceCardTaskType           `json:"type"`
-	Params      map[string]interface{} `json:"params"`
-	SubTasks    []ServiceCardSubTaskDto       `json:"subtasks"`
+	Label       string                  `json:"label"`
+	Description string                  `json:"description"`
+	Action      string                  `json:"action"`
+	Style       ServiceCardTaskStyle    `json:"style"`
+	Type        ServiceCardTaskType     `json:"type"`
+	Params      map[string]interface{}  `json:"params"`
+	SubTasks    []ServiceCardSubTaskDto `json:"subtasks"`
 }
